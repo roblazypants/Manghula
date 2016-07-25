4a1494ed0321c21197b5d5921ced876141971cbb
Dan Brown <danrb@redpine.com>
2016-07-25 07:50:10 -0400

    update to html and style sheet.

--- app.js
+++ app.js
@@ -57,5 +57,22 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+app.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
 
 module.exports = app;
